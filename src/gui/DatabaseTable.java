4e8fe65d7cec2f423188d98216a91163d705e22e
Kamil Korenkiewicz <kkorenkiewicz1@gmail.com>
2017-01-03 10:34:08 +0100

    Wybór i wyświetlanie baz danych w postaci tabeli w oknie.

---
+++ src/gui/DatabaseTable.java
@@ -0,0 +1,71 @@
+package gui;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
+
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import com.mysql.jdbc.ResultSetMetaData;
+
+import db.DatabaseHandle;
+
+public class DatabaseTable {
+
+	private ResultSet rs;
+	private JTable table; 
+	
+	
+	public DatabaseTable(int x){
+		DatabaseHandle db = new DatabaseHandle();
+		String dbName = "publications";
+		
+		if(x==0){
+			dbName="articles";
+		}else if(x==1){
+			dbName="citations";
+		}
+		
+		try {
+			rs = db.stt.executeQuery("SELECT * FROM " + dbName);
+			table = new JTable(buildTableModel());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	private DefaultTableModel buildTableModel() throws SQLException {
+		
+	    ResultSetMetaData metaData = (ResultSetMetaData) this.rs.getMetaData();
+	
+	    // names of columns
+	    Vector<String> columnNames = new Vector<String>();
+	    int columnCount = metaData.getColumnCount();
+	    for (int column = 1; column <= columnCount; column++) {
+	        columnNames.add(metaData.getColumnName(column));
+	    }
+
+	    // data of the table
+	    Vector<Vector<Object>> data = new Vector<Vector<Object>>();
+	    while (rs.next()) {
+	        Vector<Object> vector = new Vector<Object>();
+	        for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+	            vector.add(rs.getObject(columnIndex));
+	        }
+	        data.add(vector);
+	    }
+
+	    return new DefaultTableModel(data, columnNames);
+	}
+
+	
+	public JTable getTable() {
+		return table;
+	}
+
+
+}
--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -21,9 +21,6 @@ import java.awt.Component;
 public class MainWindow {
 
 	private JFrame frame;
-	private JTable citations;
-	private JTable articles;
-	private JTable publications;
 
 	/**
 	 * Launch the application.
@@ -55,58 +52,11 @@ public class MainWindow {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.X_AXIS));
+		//frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.X_AXIS));
 		
-		publications = new JTable();
-		publications.setAlignmentY(Component.TOP_ALIGNMENT);
-		DatabaseHandle db = new DatabaseHandle();
+		DatabaseTable dbTable = new DatabaseTable(0);
 		
-		ResultSet rs=null;
-		try {
-			rs = db.stt.executeQuery("SELECT * FROM publications");
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-	    // It creates and displays the table
-	    JTable table = null;
-		try {
-			table = new JTable(buildTableModel(rs));
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-	    // Closes the Connection
-
-	    JOptionPane.showMessageDialog(null, new JScrollPane(table));
-
-		frame.getContentPane().add(publications);
-	
+		frame.add(new JScrollPane(dbTable.getTable()));
 	}
-
 	
-	public static DefaultTableModel buildTableModel(ResultSet rs) throws SQLException {
-		
-	    ResultSetMetaData metaData = (ResultSetMetaData) rs.getMetaData();
-	
-	    // names of columns
-	    Vector<String> columnNames = new Vector<String>();
-	    int columnCount = metaData.getColumnCount();
-	    for (int column = 1; column <= columnCount; column++) {
-	        columnNames.add(metaData.getColumnName(column));
-	    }
-
-	    // data of the table
-	    Vector<Vector<Object>> data = new Vector<Vector<Object>>();
-	    while (rs.next()) {
-	        Vector<Object> vector = new Vector<Object>();
-	        for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
-	            vector.add(rs.getObject(columnIndex));
-	        }
-	        data.add(vector);
-	    }
-
-	    return new DefaultTableModel(data, columnNames);
-
-	}
 }
